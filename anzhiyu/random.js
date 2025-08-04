ad49a1336e38412701875ba0cfea05a57709d490
sarybool <sarybool@aliyun.com>
2025-08-04 21:21:56 +0800

    Site updated: 2025-08-04 21:21:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/04/hello-world/"];function toRandomPost(){
+var posts=["2025/08/04/值得纪念的日子/","2025/08/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
