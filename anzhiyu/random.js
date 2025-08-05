7de96fd4c26075052eed18cfb66f30e8a4f6f31f
sarybool <sarybool@aliyun.com>
2025-08-05 11:18:47 +0800

    Site updated: 2025-08-05 11:18:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/05/终于把文章的图片换掉了，记录一下/","2025/08/04/值得纪念的日子/","2025/08/04/hello-world/"];function toRandomPost(){
+var posts=["2025/08/05/设置博文默认置顶图片/","2025/08/05/终于把文章的图片换掉了，记录一下/","2025/08/04/值得纪念的日子/","2025/08/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
