d6262c5ce55f65ef5394bbc32cf5c6b43c591065
sarybool <sarybool@aliyun.com>
2025-08-05 11:02:27 +0800

    Site updated: 2025-08-05 11:02:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/04/值得纪念的日子/","2025/08/04/hello-world/"];function toRandomPost(){
+var posts=["2025/08/05/终于把文章的图片换掉了，记录一下/","2025/08/04/值得纪念的日子/","2025/08/04/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
